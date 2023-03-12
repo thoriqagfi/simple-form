c4dd90d1024c10b044567f50b3851ffedac417b9
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2023-03-12 14:13:22 +0700

    feat: add login form

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-alert("Hello, Let's fill out a form!")
-
-var toggleForm = document.getElementById("btn")
-toggleForm.addEventListener("click", function() {
-	alert("Register successful as " + document.getElementById("name").value + "!")
+var toggleFormSignup = document.getElementById("btnSignup")
+toggleFormSignup.addEventListener("click", function() {
+	alert("REGISTER successful as " + document.getElementById("name").value + "!")
 	refreshPage()
 })
+
+var toggleFormLogin = document.getElementById("btnLogin")
+toggleFormLogin.addEventListener("click", function() {
+	alert("LOGIN successful as " + document.getElementById("name").value + "!")
+	refreshPage()
+})
+
+function refreshPage() {
+	window.location.reload()
+}
+
+function loginPage() {
+	window.location.href = "login.html"
+}
+
+function signUpPage() {
+	window.location.href = "index.html"
+}
